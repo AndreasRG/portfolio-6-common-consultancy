ac742fba4c404bb391e240f26e94414dc195c154
AndreasRG <andreas@revesz-gudmann.dk>
2024-12-03 12:54:25 +0100

    Snapshot - 03/12-2024 12:45
    Added script and express start settings

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb://localhost:27017/common-cultancy";
+
+const client = new MongoClient(uri);
+
+const database = client.db('common-cultancy');
+
+MongoClient.connect("mongodb://localhost:27017", { useUnifiedTopology: true })
+    .then((client) => {
+        console.log("Connected to MongoDB");
+        database;
+    })
+
+const classification = database.collection('classification');
+const metrics = database.collection('metrics');
+const sourcepop = database.collection('sourcepop');
+const time = database.collection('time');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
