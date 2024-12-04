f0a86f14f9351cd7b6eb1ec69f74e47fcc0f8c1f
AndreasRG <andreas@revesz-gudmann.dk>
2024-12-04 12:54:28 +0100

    Snapshot - 04/12-2024 12:54
    Beginnings of endpoints/query, added chart.js in html and script

--- index.js
+++ index.js
@@ -6,26 +6,65 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require("mongodb");
 
-const uri = "mongodb://localhost:27017/common-cultancy";
+const uri = "mongodb://localhost:27017/common-consultancy";
+const client = new MongoClient(uri, { useUnifiedTopology: true });
 
-const client = new MongoClient(uri);
-
-const database = client.db('common-cultancy');
-
-MongoClient.connect("mongodb://localhost:27017", { useUnifiedTopology: true })
-    .then((client) => {
+client.connect()
+    .then(() => {
         console.log("Connected to MongoDB!");
-    });
 
-const classification = database.collection('classification');
-const metrics = database.collection('metrics');
-const sourcepop = database.collection('sourcepop');
-const time = database.collection('time');
+        const database = client.db('common-consultancy');
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+        const classification = database.collection('classification');
+        const metrics = database.collection('metrics');
+        const sourcepop = database.collection('sourcepop');
+        const time = database.collection('time');
 
-app.get('/test', (req, res) => {
-    res.send('It works')
-})
+        const app = express();
+        app.use(express.json());
+        app.use(cors());
+
+        app.get('/test', (req, res) => {
+            res.send('It works!')
+        });
+
+        app.get('/denmark', (req, res) => {
+            const query = {country: "Denmark"}
+
+            sourcepop.find(query).toArray((err, results) => {
+                if (err) {
+                    console.error("Failed to execute query:", err);
+                    return res.status(500).json({ error: "Internal Server Error" });
+                }
+
+                res.send(results);
+                res.status(200).json(results);
+            });
+        });
+
+        app.get('/restofworld', (req, res) => {
+            const query = { country: { $ne: "Denmark" } };
+
+            sourcepop.find(query).toArray((err, results) => {
+                if (err) {
+                    console.error("Failed to execute query:", err);
+                    return res.status(500).json({ error: "Internal Server Error" });
+                }
+
+                console.log(results);
+                res.status(200).json(results);
+            });
+        });
+
+
+        app.get('/', (req, res) => {
+            res.send('It works!')
+        });
+
+        // Start the server
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+    })
+    .catch(error => console.error('Failed to connect to MongoDB', error));
+
