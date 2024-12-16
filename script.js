60e08baaa6029349ea5453f565e917245f9138a0
AndreasRG <andreas@revesz-gudmann.dk>
2024-12-16 13:07:01 +0100

    Snapshot - 16/12-2024 13:06
    Updated script and css so scrolling. Updated HTML

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const elements = document.querySelectorAll('.element');
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+            // Check for linked sections
+            const linkId = entry.target.dataset.link;
+            if (linkId) {
+                document.getElementById(linkId).classList.add('visible');
+            }
+        }
+    });
+});
+elements.forEach(element => {
+    observer.observe(element);
+});
+
 
 //Section 2 canvas:
 async function fetchData(endpoint, canvasId, chartLabel, backgroundColor, borderColor) {
